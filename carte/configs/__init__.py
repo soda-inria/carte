363c4c74fbfb28f6ee8734d55252e54094f127ef
gaetanbrison <gaetan.brison@gmail.com>
2024-07-11 06:21:31 +0200

    setting init 🌳

---
+++ carte/configs/__init__.py
@@ -0,0 +1,4 @@
+from carte.configs.carte_configs import *
+from carte.configs.directory import *
+from carte.configs.model_parameters import *
+from carte.configs.visuailization import *
---
+++ carte/scripts/__init__.py
@@ -0,0 +1,5 @@
+from carte.scripts.compile_results_singletable import *
+from carte.scripts.download_data import *
+from carte.scripts.evaluate_singletable import *
+from carte.scripts.preprocess_lm import *
+from carte.scripts.preprocess_raw import *
